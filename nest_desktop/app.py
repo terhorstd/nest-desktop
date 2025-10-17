e17daefb31b5fa3030db8d6712ca3f70594bd714
Dennis Terhorst <terhorstd@users.noreply.github.com>
2025-10-17 12:12:58 +0200

    Update main function in app.py

--- nest_desktop/app.py
+++ nest_desktop/app.py
@@ -1,15 +1,17 @@
-import http.server  # noqa
-import socketserver  # noqa
-import os  # noqa
-import sys  # noqa
-
-__all__ = ["run"]
+import http.server
+import socketserver
+import os
+import sys
 
 NEST_DESKTOP_HOST = os.environ.get("NEST_DESKTOP_HOST", "127.0.0.1")
 NEST_DESKTOP_PORT = os.environ.get("NEST_DESKTOP_PORT", 54286)
 
 
 def run(host=NEST_DESKTOP_HOST, port=NEST_DESKTOP_PORT):
+    "Start a simple http server to hand out the app."
+    if len(sys.argv) > 1:
+        host = sys.argv[1]
+        port = int(sys.argv[2])
 
     web_dir = os.path.join(os.path.dirname(__file__), "app")
     os.chdir(web_dir)
@@ -17,12 +19,3 @@ def run(host=NEST_DESKTOP_HOST, port=NEST_DESKTOP_PORT):
     Handler = http.server.SimpleHTTPRequestHandler
     with socketserver.TCPServer((host, port), Handler) as httpd:
         httpd.serve_forever()
-
-
-if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        host = sys.argv[1]
-        port = sys.argv[2]
-        run(host, int(port))
-    else:
-        run()
